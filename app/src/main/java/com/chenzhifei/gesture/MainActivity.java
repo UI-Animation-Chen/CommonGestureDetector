f0dd43cc079952d81f21bab627b3bc27787c92fa
czf <502629556@qq.com>
2019-07-28 12:16:29 +0800

    replace fields by local vars

--- app/src/main/java/com/chenzhifei/gesture/MainActivity.java
+++ app/src/main/java/com/chenzhifei/gesture/MainActivity.java
@@ -11,92 +11,105 @@ import com.chenzhifei.libgesture.TwoFingersGestureDetector;
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv;
-    private float tvWidth = -1f;
-    private float tvHeight = -1f;
-
-    private float mW = -1f;
-    private float mH = -1f;
 
     private TwoFingersGestureDetector twoFingersGestureDetector;
 
-    private float rotateDeg = 0f;
-    private float scaleFactor = 1f;
-    private float translateX = 0f;
-    private float translateY = 0f;
-
-    private int transXFactor;
-    private int transYFactor;
+    private int transXDirection;
+    private int transYDirection;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = (TextView) findViewById(R.id.tv);
+        tv = findViewById(R.id.tv);
 
         twoFingersGestureDetector = new TwoFingersGestureDetector();
         twoFingersGestureDetector.enableInertialScrolling();
         twoFingersGestureDetector.setTwoFingersGestureListener(new TwoFingersGestureDetector.TwoFingersGestureListener() {
             @Override
-            public void onDown(float downX, float downY, long downTime) {
-                if (tvWidth == -1f) {
-                    tvWidth = tv.getWidth();
-                    tvHeight = tv.getHeight();
-                }
-                if (mW == -1f) {
-                    mW = ((View)tv.getParent()).getWidth();
-                    mH = ((View)tv.getParent()).getHeight();
-                }
-            }
+            public void onDown(float downX, float downY, long downTime) {}
 
             @Override
             public void onMoved(float deltaMovedX, float deltaMovedY, long deltaMilliseconds) {
-                if (Math.abs(translateX + deltaMovedX) <= mW/2) {
-                    tv.setTranslationX(translateX += deltaMovedX);
+                if (Math.abs(tv.getTranslationX() + deltaMovedX) <= getTransXLimit()) {
+                    tv.setTranslationX(tv.getTranslationX() + deltaMovedX);
                 }
-                if (Math.abs(translateY + deltaMovedY) <= mH/2) {
-                    tv.setTranslationY(translateY += deltaMovedY);
+                if (Math.abs(tv.getTranslationY() + deltaMovedY) <= getTransYLimit()) {
+                    tv.setTranslationY(tv.getTranslationY() + deltaMovedY);
                 }
             }
 
             @Override
             public void onRotated(float deltaRotatedDeg, long deltaMilliseconds) {
-                tv.setRotation(rotateDeg += deltaRotatedDeg);
+                tv.setRotation(tv.getRotation() + deltaRotatedDeg);
             }
 
             @Override
             public void onScaled(float deltaScaledX, float deltaScaledY, float deltaScaledDistance, long deltaMilliseconds) {
-                scaleFactor += deltaScaledDistance / tvWidth;
+                float scaleFactor = tv.getScaleX();
+                scaleFactor += deltaScaledDistance / tv.getWidth();
+                if (scaleFactor < 1) {
+                    scaleFactor = 1;
+                }
                 tv.setScaleX(scaleFactor);
                 tv.setScaleY(scaleFactor);
             }
 
             @Override
             public void onUp(float upX, float upY, long upTime, float xVelocity, float yVelocity) {
-                transXFactor = xVelocity > 0 ? 1 : -1;
-                transYFactor = yVelocity > 0 ? 1 : -1;
+                setReboundFactor(xVelocity, yVelocity);
             }
 
             @Override
             public void onInertialScrolling(float deltaMovedX, float deltaMovedY) {
-                if (translateX + deltaMovedX < (-mW/2)) {
-                    transXFactor = 1;
-                } else if (translateX + deltaMovedX > (mW/2)) {
-                    transXFactor = -1;
-                }
-                tv.setTranslationX(translateX += Math.abs(deltaMovedX) * transXFactor);
-
-                if (translateY + deltaMovedY < (-mH/2)) {
-                    transYFactor = 1;
-                } else if (translateY + deltaMovedY > (mH/2)) {
-                    transYFactor = -1;
-                }
-                tv.setTranslationY(translateY += Math.abs(deltaMovedY) * transYFactor);
+                reboundEffect(deltaMovedX, deltaMovedY);
             }
 
             @Override
             public void onCancel() {}
         });
+    }
+
+    private void setReboundFactor(float xVelocity, float yVelocity) {
+        transXDirection = xVelocity > 0 ? 1 : -1;
+        transYDirection = yVelocity > 0 ? 1 : -1;
+    }
+
+    private void reboundEffect(float deltaMovedX, float deltaMovedY) {
+        float transXLimit = getTransXLimit();
+        if (tv.getTranslationX() + deltaMovedX < -transXLimit) {
+            transXDirection = 1;
+        } else if (tv.getTranslationX() + deltaMovedX > transXLimit) {
+            transXDirection = -1;
+        }
+        tv.setTranslationX(tv.getTranslationX() + Math.abs(deltaMovedX) * transXDirection);
+
+        float transYLimit = getTransYLimit();
+        if (tv.getTranslationY() + deltaMovedY < -transYLimit) {
+            transYDirection = 1;
+        } else if (tv.getTranslationY() + deltaMovedY > transYLimit) {
+            transYDirection = -1;
+        }
+        tv.setTranslationY(tv.getTranslationY() + Math.abs(deltaMovedY) * transYDirection);
+    }
+
+    private float getTransXLimit() {
+        float scaledW = tv.getWidth() * tv.getScaleX();
+        return (getContainerW() - scaledW) / 2;
+    }
+
+    private float getTransYLimit() {
+        float scaledH = tv.getHeight() * tv.getScaleY();
+        return (getContainerH() - scaledH) / 2;
+    }
+
+    private int getContainerW() {
+        return ((View)tv.getParent()).getWidth();
+    }
+
+    private int getContainerH() {
+        return ((View)tv.getParent()).getHeight();
     }
 
     @Override
